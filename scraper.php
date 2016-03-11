0492034bddc037ca9cc6f039684ed6e4c539251f
searchout <searchout13b@gmail.com>
2016-03-11 17:59:38 +0900

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,12 +1,10 @@
 <?php
-// simple_html_dom.phpをインクルード
+
 include_once('simple_html_dom.php');
 
-// スクレイピングしたいURLを指定
 $html = file_get_html( "http://hotel.travel.rakuten.co.jp/hotelinfo/plan/5255?f_sort=hotel&f_dai=japan&f_teikei=quick&f_page=1&f_hyoji=100&f_image=1&f_flg=PLAN" );
 
-// 引っ張るものを指定してa.entry-link要素を$elementに代入
-foreach($html->find("a[class="htlPlnRmTypPrc roomPrice"]") as $element)
+foreach($html->find("aclass="htlPlnRmTypPrc roomPrice"]") as $element)
 
 // $element(a要素)のhrefの後ろにbrタグを入れて吐き出す
 echo $element->href . "<br>";
